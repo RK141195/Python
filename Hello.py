367bcdaca02f1475e14eb4381b96513040799f58
Ram <ramkumar.tomar@outlook.com>
2025-09-13 21:39:02 +0530

    my first github commit

--- Hello.py
+++ Hello.py
@@ -1,2 +1,9 @@
 # this is my first program in Python
 print("Hello world!")
+print("Hello again!")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print("This is my first program in Python")
